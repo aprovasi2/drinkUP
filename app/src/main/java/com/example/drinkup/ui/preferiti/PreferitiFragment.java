4c4a3b11f731d698242aa9825f380e6c0497991e
calals <72918378+calals@users.noreply.github.com>
2021-04-15 16:14:20 +0200

    modifiche ui

---
+++ app/src/main/java/com/example/drinkup/ui/preferiti/PreferitiFragment.java
@@ -0,0 +1,35 @@
+package com.example.drinkup.ui.preferiti;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.drinkup.R;
+
+public class PreferitiFragment extends Fragment {
+
+    private PreferitiViewModel preferitiViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        preferitiViewModel =
+                new ViewModelProvider(this).get(PreferitiViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_preferiti, container, false);
+        final TextView textView = root.findViewById(R.id.text_preferiti);
+        preferitiViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
