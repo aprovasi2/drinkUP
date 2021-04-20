d6a82f2f3500d5d6222e574f69d21beb8f9fc81c
calals <72918378+calals@users.noreply.github.com>
2021-04-20 14:47:54 +0200

    modifiche layout home

--- app/src/main/java/com/example/drinkup/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/drinkup/ui/home/HomeFragment.java
@@ -22,7 +22,7 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_h2, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         /*
         final TextView textView = root.findViewById(R.id.text_home);
