9ad54fed60bb56701fe1f595d59a7baf9df61eea
aprovasi2 <a.provasi2@campus.unimib.it>
2021-04-21 15:53:45 +0200

    Versione 1.0 activityDrink

--- app/src/main/java/com/example/drinkup/ActivityDrink.java
+++ app/src/main/java/com/example/drinkup/ActivityDrink.java
@@ -1,11 +1,17 @@
 package com.example.drinkup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 
 import android.content.Intent;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ActivityDrink extends AppCompatActivity {
 
@@ -13,6 +19,7 @@ public class ActivityDrink extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
 
     }
 }
