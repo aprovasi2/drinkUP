e1539e014976b93c67e51eaf889e5e954dfa47ea
aprovasi2 <a.provasi2@campus.unimib.it>
2021-04-29 16:25:25 +0200

    test gps

--- app/src/main/java/com/example/drinkup/MainActivity.java
+++ app/src/main/java/com/example/drinkup/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.drinkup;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,8 +28,45 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    public static double longitude;
+    public static double latitude;
+    private GoogleMap googleMap;
+    private String providerId = LocationManager.GPS_PROVIDER;
+    private Geocoder geo = null;
+    private LocationManager locationManager = null;
+    private static final int MIN_DIST = 20;
+    private static final int MIN_PERIOD = 30000;
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+// attivo GPS su dispositivo
+            // updateText(R.id.enabled, "TRUE");
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+// disattivo GPS su dispositivo
+            //updateText(R.id.enabled, "FALSE");
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // updateGUI(location);
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +85,77 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
 
 
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        geo = new Geocoder(this, Locale.getDefault());
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null)
+            updateGUI(location);
+        if (locationManager != null && locationManager.isProviderEnabled(providerId))
+            //updateText(R.id.enabled, "TRUE");
+
+
+            locationManager.requestLocationUpdates(providerId, MIN_PERIOD, MIN_DIST, locationListener);
+    }
+
+    private void updateGUI(Location location) {
+        Date timestamp = new Date(location.getTime());
+        //updateText(R.id.timestamp, timestamp.toString());
+        double latitude = location.getLatitude();
+        // updateText(R.id.latitude, String.valueOf(latitude));
+        double longitude = location.getLongitude();
+        //updateText(R.id.longitude, String.valueOf(longitude));
+        new AddressSolver().execute(location);
+    }
+
+
+    public class AddressSolver extends AsyncTask<Location, Void, String> {
+        @Override
+        protected String doInBackground(Location... params) {
+            Location pos = params[0];
+            double latitude = pos.getLatitude();
+            double longitude = pos.getLongitude();
+            List<Address> addresses = null;
+            try {
+                addresses = geo.getFromLocation(latitude, longitude, 1);
+            } catch (IOException e) {
+            }
+            if (addresses != null) {
+                if (addresses.isEmpty()) {
+                    return null;
+                } else {
+                    if (addresses.size() > 0) {
+                        StringBuffer address = new StringBuffer();
+                        Address tmp = addresses.get(0);
+                        for (int y = 0; y < tmp.getMaxAddressLineIndex(); y++)
+                            address.append(tmp.getAddressLine(y) + "\n");
+                        return address.toString();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+       /* if (result!=null)
+           // updateText(R.id.where, result);
+        else
+            updateText(R.id.where, "N.A.");*/
+        }
     }
 }
--- app/src/main/java/com/example/drinkup/ui/maps/MapsFragment.java
+++ app/src/main/java/com/example/drinkup/ui/maps/MapsFragment.java
@@ -1,21 +1,30 @@
 package com.example.drinkup.ui.maps;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.drinkup.MainActivity;
 import com.example.drinkup.R;
-import com.example.drinkup.ui.home.HomeViewModel;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -29,10 +38,19 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Date;
+import java.util.Locale;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
 public class MapsFragment extends Fragment {
 
+
+
     MapView mMapView;
+
     private GoogleMap googleMap;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -60,14 +78,18 @@ public class MapsFragment extends Fragment {
                 // For dropping a marker at a point on the Map
                 LatLng sydney = new LatLng(-34, 151);
                 LatLng milan = new LatLng(45.464664, 9.188540);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+                LatLng mia = new LatLng(MainActivity.latitude, MainActivity.longitude);
+                Toast.makeText(getContext(), " "+MainActivity.latitude, Toast.LENGTH_LONG).show();
+
+                googleMap.addMarker(new MarkerOptions().position(mia).title("Marker Title").snippet("Marker Description"));
                 googleMap.getUiSettings().isScrollGesturesEnabled();
                 googleMap.getUiSettings().isZoomControlsEnabled();
                 googleMap.getUiSettings().setZoomControlsEnabled(true);
+                googleMap.getUiSettings().setMapToolbarEnabled(true);
 
 
                 // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(milan).zoom(20).build();
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(mia).zoom(20).build();
 
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
@@ -79,6 +101,7 @@ public class MapsFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+
         mMapView.onResume();
     }
 
