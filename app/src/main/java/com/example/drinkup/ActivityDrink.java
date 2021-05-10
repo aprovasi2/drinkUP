a19bc71437ea8ed9e192a1d73638ca72c1eae4d0
aprovasi2 <a.provasi2@campus.unimib.it>
2021-05-10 11:04:18 +0200

    risolto problema navigation bar

--- app/src/main/java/com/example/drinkup/ActivityDrink.java
+++ app/src/main/java/com/example/drinkup/ActivityDrink.java
@@ -9,7 +9,10 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +20,16 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -27,6 +39,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashSet;
 
 public class ActivityDrink extends AppCompatActivity implements View.OnClickListener {
 
@@ -34,10 +47,13 @@ public class ActivityDrink extends AppCompatActivity implements View.OnClickList
     private EditText drinkDaCercare;
     private TextView nomeDrink;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
 
         button_Search = (Button) findViewById(R.id.button_Search);
         button_Search.setOnClickListener(this);
@@ -45,49 +61,50 @@ public class ActivityDrink extends AppCompatActivity implements View.OnClickList
         nomeDrink = (TextView) findViewById(R.id.textView_Alchool_Drink);
         drinkDaCercare = (EditText) findViewById(R.id.editTextText_DrinkSearch);
 
+
     }
+
 
     @Override
     public void onClick(View v) {
 
         String ricerca = drinkDaCercare.getText().toString();
-        Toast toast = Toast.makeText(this, ricerca, Toast.LENGTH_LONG);
-        //toast.show();
+        Toast toast = Toast.makeText(this, "ho provato a ricercare "+ricerca, Toast.LENGTH_LONG);
+        toast.show();
 
-        // Creao l'oggetto URL che rappresenta l'indirizzo della pagina da richiamare
-        URL paginaURL = null;
-        try {
-            paginaURL = new URL("www.thecocktaildb.com/api/json/v1/1/search.php?s=" + ricerca);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            Toast toast1 = Toast.makeText(this, "try1", Toast.LENGTH_LONG);
-            toast1.show();
-        }
 
-        // creo l'oggetto HttpURLConnection e apro la connessione al server
-        HttpURLConnection client = null;
+        /*
+        InputStream risposta= null ;
+
         try {
-            client = (HttpURLConnection) paginaURL.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast toast2 = Toast.makeText(this, "try2", Toast.LENGTH_LONG);
+
+            // Creao l'oggetto URL che rappresenta l'indirizzo della pagina da richiamare
+            URL paginaURL = new URL("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + ricerca);
+            Toast toast2 = Toast.makeText(this, "la url è  "+paginaURL, Toast.LENGTH_LONG);
             toast2.show();
-        }
 
-        // Recupero le informazioni inviate dal server
-        try {
-            InputStream risposta = new BufferedInputStream(client.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast toast3 = Toast.makeText(this, "try3", Toast.LENGTH_LONG);
+            // creo l'oggetto HttpURLConnection e apro la connessione al server
+            HttpURLConnection client = (HttpURLConnection) paginaURL.openConnection();
+
+            // Recupero le informazioni inviate dal server
+             risposta = new BufferedInputStream(client.getInputStream());
+
+        } catch (Exception e) {
+            Toast toast3 = Toast.makeText(this, "sono in eccezione ", Toast.LENGTH_LONG);
             toast3.show();
+
+            e.printStackTrace();
         }
 
-        //nomeDrink.setText(mostroDati(risposta));
-        //Toast toast = Toast.makeText(this, "ciao", Toast.LENGTH_LONG);
-        //toast.show();
+
+       String datiLetti = mostroDati(risposta);
+        Toast toast3 = Toast.makeText(this, "i dati letti sono "+datiLetti, Toast.LENGTH_LONG);
+        toast3.show();*/
 
     }
+
+
+
 
     private static String mostroDati(InputStream in) {
         StringBuilder sb = new StringBuilder();
@@ -102,4 +119,6 @@ public class ActivityDrink extends AppCompatActivity implements View.OnClickList
 
         return sb.toString();
     }
+
+
 }
